96e941ed0b43f85622e81579b056b8a5c1c3d8ca
Tyler Lane <sld.potato@gmail.com>
2020-02-14 11:09:19 -0600

    Multiple origins for CORS

--- api/environment-variables.js
+++ api/environment-variables.js
@@ -1,8 +1,10 @@
 let dotenv = require('dotenv').config()
 
-let expectedEnvironmentVariables = ['PORT', 'SESSION_SECRET', 'PROMO_CODE']
+let expectedEnvironmentVariables = ['PORT', 'SESSION_SECRET', 'PROMO_CODE', 'ALLOW_ORIGIN_URLS']
 for(expectedEnvironmentVariable of expectedEnvironmentVariables) {
   if(process.env[expectedEnvironmentVariable] == undefined) {
     throw new Error(`Environment variable ${expectedEnvironmentVariable} not defined`)
   }
 }
+
+process.env.ALLOW_ORIGIN_URLS = process.env.ALLOW_ORIGIN_URLS.split(',')
--- api/middleware/cors.js
+++ api/middleware/cors.js
@@ -5,10 +5,14 @@ let cors = function(request, response, next) {
   response.header("X-Content-Type-Options", "nosniff")
   response.header("X-Permitted-Cross-Domain-Policies", "none")
 
-  response.header("Access-Control-Allow-Origin", "http://localhost:8080")
   response.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
   response.header("Access-Control-Allow-Credentials", "true")
   response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+
+  // Allow from multiple origins
+  let origin = request.get('origin')
+  if(process.env.ALLOW_ORIGIN_URLS.includes(origin))
+    response.header("Access-Control-Allow-Origin", origin)
 
   // fuckin optionzz
   if(request.method == 'OPTIONS') response.end()
